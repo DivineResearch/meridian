0a21f546a9c34c51e4e8f0376ae3c7c6a1d02192
scammi <scammi@gmail.com>
2026-01-29 20:06:28 -0300

    chore: format and comments guidelines

--- src/LockManager.sol
+++ src/LockManager.sol
@@ -1,41 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
-import { ILockManager } from "./interfaces/ILockManager.sol";
-import { ISignatureTransfer } from "../lib/permit2/src/interfaces/ISignatureTransfer.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
+import {ISignatureTransfer} from "permit2/interfaces/ISignatureTransfer.sol";
+
+import {ILockManager} from "./interfaces/ILockManager.sol";
+
 /// @title LockManager
-/// @notice Shared lock registry for Permit2 coordination
+/// @author Divine Research
+/// @notice Shared lock registry for coordinating exclusive access to user funds via Permit2
 contract LockManager is ILockManager, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                             STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Permit2 contract for signature-based transfers
     ISignatureTransfer public immutable PERMIT2;
 
+    /// @notice Lock information for each user
     mapping(address user => Lock) internal _locks;
+
+    /// @notice Authorization status for each partner
     mapping(address partner => bool) internal _partners;
 
+    /*//////////////////////////////////////////////////////////////
+                                MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Ensure only authorized partners can call
     modifier onlyPartner() {
         if (!_partners[msg.sender]) revert NotAuthorized();
         _;
     }
 
-    constructor(
-        address initialOwner,
-        address permit2
-    ) Ownable(initialOwner) {
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Initialize the LockManager contract
+    /// @param initialOwner Address of the contract owner
+    /// @param permit2 Address of the Permit2 contract
+    constructor(address initialOwner, address permit2) Ownable(initialOwner) {
         PERMIT2 = ISignatureTransfer(permit2);
     }
 
+    /*//////////////////////////////////////////////////////////////
+                             CORE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ILockManager
     function lock(address user, uint40 expiration) external onlyPartner {
         if (isLocked(user)) revert LockActive();
 
         _locks[user] = Lock(msg.sender, expiration);
+
         emit LockCreated(user, msg.sender, expiration);
     }
 
+    /// @inheritdoc ILockManager
     function release(address user) external {
         revert NotHolder();
     }
 
+    /// @inheritdoc ILockManager
     function execute(
         ISignatureTransfer.PermitTransferFrom calldata permit,
         ISignatureTransfer.SignatureTransferDetails calldata transferDetails,
@@ -45,20 +73,35 @@ contract LockManager is ILockManager, Ownable {
         revert NotHolder();
     }
 
+    /*//////////////////////////////////////////////////////////////
+                            ADMIN FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Set the authorization status of a partner
+    /// @param partner Address of the partner
+    /// @param status New authorization status
     function setPartnerStatus(address partner, bool status) external onlyOwner {
         _partners[partner] = status;
+
         emit PartnerUpdated(partner, status);
     }
 
+    /*//////////////////////////////////////////////////////////////
+                            GETTER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ILockManager
     function getLock(address user) external view returns (Lock memory) {
         return _locks[user];
     }
 
+    /// @inheritdoc ILockManager
     function isLocked(address user) public view returns (bool) {
         Lock memory userLock = _locks[user];
         return userLock.holder != address(0) && userLock.expiresAt > block.timestamp;
     }
 
+    /// @inheritdoc ILockManager
     function isPartner(address account) external view returns (bool) {
         return _partners[account];
     }
--- src/interfaces/ILockManager.sol
+++ src/interfaces/ILockManager.sol
@@ -1,28 +1,92 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
-import {ISignatureTransfer} from "../../lib/permit2/src/interfaces/ISignatureTransfer.sol";
+import {ISignatureTransfer} from "permit2/interfaces/ISignatureTransfer.sol";
 
+/// @title ILockManager
+/// @author Divine Research
+/// @notice Interface for the LockManager contract
 interface ILockManager {
+    /*//////////////////////////////////////////////////////////////
+                                 TYPES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Lock information structure
+    /// @param holder Address that holds the lock
+    /// @param expiresAt Timestamp when the lock expires
     struct Lock {
         address holder;
         uint40 expiresAt;
     }
 
-    event LockCreated(address indexed user, address indexed holder, uint40 expiresAt);
-    event LockReleased(address indexed user, address indexed holder);
-    event PermitExecuted(address indexed user, address indexed holder, address token, uint256 amount, address recipient);
-    event PartnerUpdated(address indexed partner, bool status);
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
 
+    /// @notice Thrown when caller is not an authorized partner
     error NotAuthorized();
+
+    /// @notice Thrown when caller is not the lock holder
     error NotHolder();
+
+    /// @notice Thrown when user already has an active lock
     error LockActive();
+
+    /// @notice Thrown when user has no active lock
     error NoActiveLock();
+
+    /// @notice Thrown when expiration is invalid
     error InvalidExpiration();
+
+    /// @notice Thrown when address is invalid (zero address)
     error InvalidAddress();
 
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when a lock is created
+    /// @param user Address of the user whose funds are locked
+    /// @param holder Address that holds the lock
+    /// @param expiresAt Timestamp when the lock expires
+    event LockCreated(address indexed user, address indexed holder, uint40 expiresAt);
+
+    /// @notice Emitted when a lock is released
+    /// @param user Address of the user whose lock was released
+    /// @param holder Address that held the lock
+    event LockReleased(address indexed user, address indexed holder);
+
+    /// @notice Emitted when a permit is executed
+    /// @param user Address of the user whose funds were transferred
+    /// @param holder Address that executed the permit
+    /// @param token Address of the token transferred
+    /// @param amount Amount of tokens transferred
+    /// @param recipient Address that received the tokens
+    event PermitExecuted(address indexed user, address indexed holder, address token, uint256 amount, address recipient);
+
+    /// @notice Emitted when partner status is updated
+    /// @param partner Address of the partner
+    /// @param status New status of the partner
+    event PartnerUpdated(address indexed partner, bool status);
+
+    /*//////////////////////////////////////////////////////////////
+                             CORE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Acquire exclusive lock on a user's funds
+    /// @param user Address of the user to lock
+    /// @param expiration Timestamp when the lock expires
     function lock(address user, uint40 expiration) external;
+
+    /// @notice Release lock on a user's funds
+    /// @param user Address of the user to release
     function release(address user) external;
+
+    /// @notice Execute a Permit2 transfer while holding the lock
+    /// @param permit Permit2 permit data
+    /// @param transferDetails Transfer details including recipient and amount
+    /// @param user Address of the user whose funds are being transferred
+    /// @param signature User's signature for the permit
     function execute(
         ISignatureTransfer.PermitTransferFrom calldata permit,
         ISignatureTransfer.SignatureTransferDetails calldata transferDetails,
@@ -30,7 +94,22 @@ interface ILockManager {
         bytes calldata signature
     ) external;
 
+    /*//////////////////////////////////////////////////////////////
+                            GETTER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Get lock information for a user
+    /// @param user Address of the user
+    /// @return Lock information
     function getLock(address user) external view returns (Lock memory);
+
+    /// @notice Check if a user has an active lock
+    /// @param user Address of the user
+    /// @return True if the user has an active lock
     function isLocked(address user) external view returns (bool);
+
+    /// @notice Check if an address is an authorized partner
+    /// @param account Address to check
+    /// @return True if the address is an authorized partner
     function isPartner(address account) external view returns (bool);
 }
