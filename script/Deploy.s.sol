bb199ed987c3dc2ae6a3ec8c9ce56a3c1bcf8e5c
cairo <cairoeth@protonmail.com>
2026-02-02 15:55:45 -0800

    feat: add scripts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,9 +3,28 @@ pragma solidity ^0.8.33;
 
 import {Script} from "forge-std/Script.sol";
 
-/// @title Deploy
-/// @notice Deployment script for Meridian protocol
-contract Deploy is Script {
-    // TODO: Deployment logic
+import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
+import {LockManager} from "src/LockManager.sol";
+
+/// @dev Deploy the LockManager contract
+contract Deploy is Script {
+    address internal constant PERMIT2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+
+    /// @notice Deploy contracts
+    function run() external {
+        vm.startBroadcast();
+
+        (, address deployer,) = vm.readCallers();
+
+        // Deploy the LockManager as proxy
+        new ERC1967Proxy(
+            address(new LockManager()), abi.encodeWithSignature("initialize(address,address)", deployer, PERMIT2)
+        );
+
+        vm.stopBroadcast();
     }
+
+    // Exclude from coverage report
+    function test() public {}
+}
---
+++ script/Upgrade.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.33;
+
+import {Script} from "forge-std/Script.sol";
+
+import {LockManager} from "src/LockManager.sol";
+
+/// @dev Upgrade the LockManager contract
+contract Upgrade is Script {
+    error DeployerInvalid();
+    error LockManagerOwnerInvalid();
+
+    /// @notice Upgrade contracts
+    /// @param lockManagerProxy Address of the LockManager proxy
+    function run(address lockManagerProxy) external {
+        vm.startBroadcast();
+
+        (, address deployer,) = vm.readCallers();
+
+        LockManager lockManager = LockManager(lockManagerProxy);
+        if (lockManager.owner() != deployer) revert DeployerInvalid();
+
+        // Upgrade the LockManager
+        address ownerBefore = lockManager.owner();
+
+        lockManager.upgradeToAndCall(address(new LockManager()), "");
+
+        // Validate upgrade
+        if (lockManager.owner() != ownerBefore) revert LockManagerOwnerInvalid();
+
+        vm.stopBroadcast();
+    }
+
+    // Exclude from coverage report
+    function test() public {}
+}
